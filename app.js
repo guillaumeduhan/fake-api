3d1022358fc8032ef759d75edb145834237212fc
Guillaume Duhan <myhappyagency@gmail.com>
2020-07-29 22:40:24 +0200

    App.js use routes files now

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
-app.get("/", function (req, res) {
-  res.send("Hello World!");
-});
+app.use(require("./routes"));
 
-app.listen(3000, function () {
-  console.log("Example app listening on port 3000!");
-});
+async function start() {
+  app.listen(3000, function () {
+    console.log("Launched on port 3000!");
+  });
+}
+
+start();
