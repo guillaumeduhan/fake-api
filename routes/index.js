5e5990ed097f4193f562428f992d440955582911
Guillaume Duhan <myhappyagency@gmail.com>
2020-07-29 23:10:18 +0200

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,11 @@
-var express = require("express");
-var router = express.Router();
+let express = require("express");
+let router = express.Router();
+let _ = require("lodash");
+
+const s = require("../services/user");
 
 router.get("/", function (req, res) {
-  res.send("Hello world");
-});
-
-router.get("/generate/user", function (req, res) {
+  console.log(s.generateNewUser());
   res.send("Hello world");
 });
 
